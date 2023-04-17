404c8ec7690ddc559035891bf931d637ccc676bd
Aghent47 <neiderkk@gmail.com>
2023-04-17 14:01:28 -0500

    Mostrar usuarios en pantalla

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -10,9 +10,28 @@ export const Usuarios = () => {
         //llamado al API
         reqResApi.get<ReqresListado>('/users').then(resp => {
             console.log(resp.data.data);
+            setUsuarios(resp.data.data);
         })
             .catch(console.log)
     }, [])
+
+    const renderUsuarios = ({ id, first_name, last_name, email,avatar }: Usuario) => {
+        return (
+            <tr key={ id.toString()} >
+                <td>
+                    <img src={avatar}
+                    alt={first_name} 
+                    style={{
+                        width: 35,
+                        borderRadius: 100
+                    }} />
+                </td>
+                <td> {first_name} {last_name} </td>
+                <td> {email} </td>
+            </tr>
+
+        )
+    }
 
     return (
         <>
@@ -26,12 +45,16 @@ export const Usuarios = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Neider Hernández</td>
-
-                    </tr>
+                    {
+                        usuarios.map(renderUsuarios)
+                    }
                 </tbody>
             </table>
+            <button
+                className="btn btn-primary"
+            >
+                Siguiente
+            </button>
         </>
     )
 }
